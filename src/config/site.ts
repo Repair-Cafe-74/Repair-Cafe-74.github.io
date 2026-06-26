e28074424f9d913d1c0482c1a85acd14e004b9c4
Fabrice Bellingard <bellingard@gmail.com>
2026-06-26 17:26:30 +0200

    Optimize SEO

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export const SITE = {
+    name: "Repair Café 74",
+    url: "https://www.repaircafe74.fr",
+  
+    defaultTitle: "Repair Café 74",
+  
+    description:
+      "Réparez vos objets avec les bénévoles du Repair Café 74 en Haute-Savoie.",
+  
+    locale: "fr_FR",
+  
+    language: "fr",
+  
+    defaultImage: "/images/logo_repaircafe74.png",
+  
+    author: "Repair Café 74",
+  };
